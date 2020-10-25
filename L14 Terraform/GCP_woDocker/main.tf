023d533f146d6a9985cfac1cd7d7b4a99e58b699
Azamat <azamated@gmail.com>
2020-10-25 23:04:05 +0300

    "Lesson 14 - final, delay between provisioning

--- L14 Terraform/GCP_woDocker/main.tf
+++ L14 Terraform/GCP_woDocker/main.tf
@@ -87,18 +87,11 @@ resource "google_compute_firewall" "default" {
 
 }
 
-# This resource will destroy (potentially immediately) after null_resource.next
-resource "null_resource" "previous" {}
 
 resource "time_sleep" "wait_30_seconds" {
-  depends_on = [null_resource.previous]
+  depends_on = [google_compute_instance.vm_instance1]
 
   create_duration = "30s"
-}
-
-# This resource will create (at least) 30 seconds after null_resource.previous
-resource "null_resource" "next" {
-  depends_on = [time_sleep.wait_30_seconds]
 }
 
 #################
