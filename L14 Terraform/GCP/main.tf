b37e4abe9739d5b2199db64c579b7d3fcb6def0d
Azamat <azamated@gmail.com>
2020-10-25 13:51:32 +0300

    Lesson 14 - I2 instances ubuntu

--- L14 Terraform/GCP/main.tf
+++ L14 Terraform/GCP/main.tf
@@ -18,7 +18,28 @@ resource "google_compute_instance" "vm_instance" {
     protocol = "all"
   }
 
-  source_tags = ["web"]
+  network_interface {
+    # A default network is created for all GCP projects
+    network = "default"
+    access_config {
+    }
+  }
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "ubuntu-production"
+  machine_type = "e2-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-1804-bionic-v20201014"
+    }
+  }
+
+   allow {
+    protocol = "all"
+  }
+
   network_interface {
     # A default network is created for all GCP projects
     network = "default"
