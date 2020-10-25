8103c4c45cd97171bebb248e7764e1d0264daf8e
Azamat <azamated@gmail.com>
2020-10-25 13:28:55 +0300

    Lesson 14 - Init

---
+++ L14 Terraform/main.tf
@@ -0,0 +1,23 @@
+provider "google" {
+  project = "tough-history-289605"
+  region  = "us-central1"
+  zone    = "us-central1-a"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "ubuntu_builder"
+  machine_type = "e2-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-1804-bionic-v20201014"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network = "default"
+    access_config {
+    }
+  }
+}
