cca26053c809993e424eb9ede8d8d67f7131c35f
Azamat <azamated@gmail.com>
2020-10-25 16:12:52 +0300

    Lesson 14 - package installation and transfer to CLS

--- L14 Terraform/GCP/main.tf
+++ L14 Terraform/GCP/main.tf
@@ -4,9 +4,44 @@ provider "google" {
   zone    = "us-central1-a"
 }
 
+# Builder node
 resource "google_compute_instance" "vm_instance1" {
   name         = "ubuntu-builder"
-  machine_type = "e2-micro"
+  machine_type = "e2-standard"
+
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-1804-bionic-v20201014"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+  provisioner "install packages" {
+    command = "apt-get update; apt-get install -y docker.io && apt-get install -y maven && apt-get install -y git"
+  }
+
+  provisioner "build javac" {
+    command = "cd /tmp && git clone https://github.com/azamated/boxfuse-sample-java-war-hello.git; mvn package -f /tmp/boxfuse-sample-java-war-hello"
+  }
+
+  provisioner "build docker image" {
+    command = "docker build -f /tmp/boxfuse-sample-java-war-hello/Dockerfile -t boxfusewebapp /tmp/boxfuse-sample-java-war-hello"
+  }
+
+  }
+
+# Production node
+resource "google_compute_instance" "vm_instance2" {
+  name         = "ubuntu-production"
+  machine_type = "e2-standard"
 
   boot_disk {
     initialize_params {
@@ -23,9 +58,32 @@ resource "google_compute_instance" "vm_instance1" {
     }
   }
 
-  metadata_startup_script = "apt-get update; apt-get install -y docker.io && apt-get install -y maven && apt-get install -y git; cd /tmp && git clone https://github.com/azamated/boxfuse-sample-java-war-hello.git && mvn package -f /tmp/boxfuse-sample-java-war-hello; docker build -f /tmp/boxfuse-sample-java-war-hello/Dockerfile -t boxfusewebapp /tmp/boxfuse-sample-java-war-hello"
+  provisioner "install packages" {
+    command = "apt-get update; apt-get install -y docker.io && apt-get install -y git"
   }
 
+}
+
+# Network provisioning
+resource "google_compute_firewall" "default" {
+  name    = "instance-firewall"
+  network = "default"
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "8080", "22"]
+  }
+
+}
+resource "google_compute_network" "default" {
+  name = "instance-network"
+}
+
+#Copy war artifact to Google Storage bucket
 resource "google_storage_bucket_object" "war-file" {
   name   = "hello-1.0.war"
   source = "/tmp/boxfuse-sample-java-war-hello/target/hello-1.0.war"
@@ -42,45 +100,7 @@ resource "google_storage_bucket" "bucket" {
   name = "static-content-bucket"
 }
 
-resource "google_compute_instance" "vm_instance2" {
-  name         = "ubuntu-production"
-  machine_type = "e2-micro"
 
-  boot_disk {
-    initialize_params {
-      image = "ubuntu-1804-bionic-v20201014"
-    }
-  }
 
-  network_interface {
-    # A default network is created for all GCP projects
-    network = "default"
-
-    access_config {
-  // Ephemeral IP
-    }
-  }
-
-  metadata_startup_script = "apt-get update; apt-get install -y docker.io && apt-get install tomcat8 -y && cd /usr/local/tomcat/webapps/ && wget https://storage.googleapis.com/aamirakulov/hello-1.0.war"
-}
-
-resource "google_compute_firewall" "default" {
-  name    = "instance-firewall"
-  network = "default"
-
-  allow {
-    protocol = "icmp"
-  }
-
-  allow {
-    protocol = "tcp"
-    ports    = ["80", "8080", "22"]
-  }
-
-}
-
-resource "google_compute_network" "default" {
-  name = "instance-network"
-}
 
 
