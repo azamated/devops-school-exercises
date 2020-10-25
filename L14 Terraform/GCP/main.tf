1a0ffb5fccea6af4a310dc99c0e7d98220686f61
Azamat <azamated@gmail.com>
2020-10-25 17:17:53 +0300

    Lesson 14 - added bucket

--- L14 Terraform/GCP/main.tf
+++ L14 Terraform/GCP/main.tf
@@ -6,7 +6,7 @@ provider "google" {
 
 # Builder node
 resource "google_compute_instance" "vm_instance1" {
-  name         = "ubuntu-builder2"
+  name         = "ubuntu-builder3"
   machine_type = "e2-micro"
 
 
@@ -26,7 +26,7 @@ resource "google_compute_instance" "vm_instance1" {
   }
 
   provisioner "local-exec" {
-    command = "sudo apt-get update && sudo apt-get install -y docker.io && sudo apt-get install -y maven && sudo apt-get install -y default-jdk && sudo apt-get install -y git"
+    command = "sudo apt-get update && sudo apt-get install -y docker.io maven default-jdk gsutil"
 
   }
 
@@ -35,13 +35,18 @@ resource "google_compute_instance" "vm_instance1" {
 
   }
 
-    provisioner "local-exec" {
+  provisioner "local-exec" {
     command = "sudo mvn package -f /tmp/boxfuse-sample-java-war-hello"
 
   }
 
+  provisioner "local-exec" {
+    command = "sudo gsutil cp /tmp/boxfuse-sample-java-war-hello/target/hello-1.0.war gs://aamirakulov/"
+
   }
 
+
+  }
 
 # Firewall rules
 resource "google_compute_firewall" "default" {
@@ -59,46 +64,9 @@ resource "google_compute_firewall" "default" {
 
 }
 
-#Copy war artifact to Google Storage bucket
-resource "google_storage_bucket_object" "war-file" {
-  name   = "java-webapp-prod.war"
-  source = "/tmp/boxfuse-sample-java-war-hello/target/hello-1.0.war"
-  bucket = "aamirakulov"
-}
 
-#################
-# Production node
-#################
-resource "google_compute_instance" "vm_instance2" {
-  name         = "ubuntu-production2"
-  machine_type = "e2-micro"
 
-  boot_disk {
-    initialize_params {
-      image = "ubuntu-1804-bionic-v20201014"
-    }
-  }
 
-  network_interface {
-    # A default network is created for all GCP projects
-    network = "default"
-
-    access_config {
-  // Ephemeral IP
-    }
-  }
-
-  provisioner "local-exec" {
-    command = "sudo apt-get update && sudo apt-get install -y docker.io && sudo apt-get install -y git && sudo apt-get install -y default-jdk && sudo apt-get install -y tomcat8"
-
-  }
-
-  provisioner "local-exec" {
-    command = "cd /tmp && wget https://storage.googleapis.com/aamirakulov/java-webapp-prod.war && sudo cp java-webapp-prod.war /usr/local/tomcat/webapps/"
-
-  }
-
-}
 
 
 
