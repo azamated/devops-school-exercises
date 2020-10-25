742f81a5de599862e63b740739171ea374aa9457
Azamat <azamated@gmail.com>
2020-10-25 16:54:32 +0300

    Lesson 14 - added bucket

--- L14 Terraform/GCP/main.tf
+++ L14 Terraform/GCP/main.tf
@@ -27,17 +27,17 @@ resource "google_compute_instance" "vm_instance1" {
 
   provisioner "local-exec" {
     command = "sudo apt-get update && sudo apt-get install -y docker.io && sudo apt-get install -y maven && sudo apt-get install -y git"
-    on_failure = continue
+
   }
 
   provisioner "local-exec" {
     command = "cd /tmp && git clone https://github.com/azamated/boxfuse-sample-java-war-hello.git && sudo mvn package -f /tmp/boxfuse-sample-java-war-hello"
-    on_failure = continue
+
   }
 
   provisioner "local-exec" {
     command = "sudo docker build -f /tmp/boxfuse-sample-java-war-hello/Dockerfile -t boxfusewebapp /tmp/boxfuse-sample-java-war-hello"
-    on_failure = continue
+
   }
 
   }
@@ -90,12 +90,12 @@ resource "google_compute_instance" "vm_instance2" {
 
   provisioner "local-exec" {
     command = "sudo apt-get update && sudo apt-get install -y docker.io && sudo apt-get install -y git && sudo apt-get install -y default-jdk && sudo apt-get install -y tomcat8"
-    on_failure = continue
+
   }
 
   provisioner "local-exec" {
     command = "cd /tmp && wget https://storage.googleapis.com/aamirakulov/java-webapp-prod.war && sudo cp java-webapp-prod.war /usr/local/tomcat/webapps/"
-    on_failure = continue
+
   }
 
 }
