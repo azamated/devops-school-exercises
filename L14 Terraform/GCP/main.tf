a24a684960e162f62d4aaf204bc3a16182fb05f6
Azamat <azamated@gmail.com>
2020-10-25 16:27:17 +0300

    Lesson 14 - just 1 instance

--- L14 Terraform/GCP/main.tf
+++ L14 Terraform/GCP/main.tf
@@ -38,33 +38,8 @@ resource "google_compute_instance" "vm_instance1" {
 
   }
 
-# Production node
-resource "google_compute_instance" "vm_instance2" {
-  name         = "ubuntu-production"
-  machine_type = "e2-micro"
 
-  boot_disk {
-    initialize_params {
-      image = "ubuntu-1804-bionic-v20201014"
-    }
-  }
-
-  network_interface {
-    # A default network is created for all GCP projects
-    network = "default"
-
-    access_config {
-  // Ephemeral IP
-    }
-  }
-
-  provisioner "local-exec" {
-    command = "apt-get update; apt-get install -y docker.io && apt-get install -y git"
-  }
-
-}
-
-# Network provisioning
+# Firewall rules
 resource "google_compute_firewall" "default" {
   name    = "instance-firewall"
   network = "default"
@@ -79,25 +54,12 @@ resource "google_compute_firewall" "default" {
   }
 
 }
-resource "google_compute_network" "default" {
-  name = "instance-network"
-}
 
 #Copy war artifact to Google Storage bucket
 resource "google_storage_bucket_object" "war-file" {
   name   = "hello-1.0.war"
   source = "/tmp/boxfuse-sample-java-war-hello/target/hello-1.0.war"
   bucket = "aamirakulov"
-}
-
-resource "google_storage_bucket_access_control" "public_rule" {
-  bucket = "aamirakulov"
-  role   = "READER"
-  entity = "allUsers"
-}
-
-resource "google_storage_bucket" "bucket" {
-  name = "static-content-bucket"
 }
 
 
