52a53fd99bd6b3ba2836617c546b0e05c8bcaa7e
Azamat <azamated@gmail.com>
2020-10-30 21:10:30 +0300

    Final project - TF config

--- Final Project/Terraform/main.tf
+++ Final Project/Terraform/main.tf
@@ -7,30 +7,32 @@ terraform {
   }
 }
 
-##############################################
+######################
 #Create aws instances#
-##############################################
+######################
+
 provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_instance" "builder1" {
+resource "aws_instance" "builder3" {
   ami = "ami-07efac79022b86107"
   instance_type = "t2.micro"
-  vpc_security_group_ids = ["${aws_security_group.build_allow_ssh.id}"]
+  vpc_security_group_ids = [aws_security_group.build_allow_ssh.id]
 
 }
 
-resource "aws_instance" "production2" {
+resource "aws_instance" "production3" {
   ami = "ami-07efac79022b86107"
   instance_type = "t2.micro"
-  vpc_security_group_ids = ["${aws_security_group.prod_allow_ssh_web.id}"]
+  vpc_security_group_ids = [aws_security_group.prod_allow_ssh_web.id]
 }
 
 
 ##############################################
 #Create security groups with FW ports allowed#
 ##############################################
+
 resource "aws_security_group" "build_allow_ssh" {
   name = "allow_ssh"
   description = "Allow SSH inbound traffic"
@@ -41,6 +43,12 @@ resource "aws_security_group" "build_allow_ssh" {
     protocol = "tcp"
     cidr_blocks = [
       "0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
@@ -62,5 +70,11 @@ resource "aws_security_group" "prod_allow_ssh_web" {
     protocol = "tcp"
     cidr_blocks = [
       "0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
