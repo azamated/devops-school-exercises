946a0823ba3de94a738643d03f006ff4966776b7
Azamat <azamated@gmail.com>
2020-10-30 22:24:58 +0300

    Final project - TF config

--- Final Project/Terraform/main.tf
+++ Final Project/Terraform/main.tf
@@ -7,6 +7,10 @@ terraform {
   }
 }
 
+
+env AWS_ACCESS_KEY_ID = "${AWS_ACCESS_KEY_ID}"
+env AWS_SECRET_ACCESS_KEY = "${AWS_SECRET_ACCESS_KEY}"
+
 resource "aws_key_pair" "id_rsa" {
   key_name   = "aws_id_rsa_pub"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC8LHlRNOITkvCFipJu/GFBVICUEXEtZJBawqJyKk8frjIeSvdgpZSfOxdgRDHUQ8XWote6QxSXsp4f1JtBWJNAagKI0y14egh0mxAMtzcPkSrNR4IxjmMEFBS70tgLyg13V4FStK2rrcRPwpmttdD0/bys3FALk7yTIlGfqpEH4RN2qk+xkLR6HMwXPFEDtqt9KyIND84F8TjwxOZGahKlia5kS2W+9V3L6fn3KuSf36vIrLahvgQsYIQeBgaIKtlDlhqHUzOz/+Y1E22T08OuqswqwEQdvlfybchGc/68gYlkNib3BKGIAtbJY4h6ynPKdPcEK01AUHxj6fy6nynHABBrSJtlqcgbJfGaZxoRl0AwTWH3DuEHNgqFyDdr54eStHEfmatNdqf21VdqqegNQaw8AX0LUWCzp5sZ4NzuN25+RRoVjtFaZjWINoVimqWqRVntatW1LDNTnA05BRad1eAp37D9Js1yqT/8ilWmH/UO8EsujQvEagvspcevXI0= root@devops"
@@ -27,6 +31,18 @@ resource "aws_instance" "builder3" {
   key_name = "aws_id_rsa_pub"
   vpc_security_group_ids = [aws_security_group.build_allow_ssh.id]
 
+  #Copies aws cred file to the instance
+  provisioner "file" {
+    source      = "~/.aws/credentials"
+    destination = "~/.aws/credentials"
+
+    connection {
+      type = "ssh"
+      user = "root"
+      private_key = "${file("~/.ssh/id_rsa")}"
+      agent = "false"
+    }
+  }
 }
 
 resource "aws_instance" "production3" {
@@ -35,6 +51,18 @@ resource "aws_instance" "production3" {
   monitoring = true
   key_name = "aws_id_rsa_pub"
   vpc_security_group_ids = [aws_security_group.prod_allow_ssh_web.id]
+  #Copies aws cred file to the instance
+  provisioner "file" {
+    source      = "~/.aws/credentials"
+    destination = "~/.aws/credentials"
+
+    connection {
+      type = "ssh"
+      user = "root"
+      private_key = "${file("~/.ssh/id_rsa")}"
+      agent = "false"
+    }
+  }
 }
 
 
@@ -87,3 +115,8 @@ resource "aws_security_group" "prod_allow_ssh_web" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+##############
+#Provisioners#
+##############
+
